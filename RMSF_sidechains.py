1bdf1dae6ab3e710b51adbc9ef30c70f183073c8
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-02-22 19:29:37 +0000

    RMSF_sidechains.py

--- RMSF_sidechains.py
+++ RMSF_sidechains.py
@@ -2,16 +2,9 @@ import mdtraj as md
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load trajectory and topology
-trajectory = md.load('trajectory.dcd', top='topology.pdb')
-
-# Select side-chain atoms for residues 12 to 328
+trajectory = md.load('t.dcd', top='t.pdb')
 atom_indices = trajectory.top.select('sidechain and resid 12 to 328')
-
-# Calculate RMSF
 rmsf = md.rmsf(trajectory, atom_indices=atom_indices)
-
-# Plot RMSF
 plt.plot(rmsf, label='RMSF')
 plt.xlabel('Residue Index')
 plt.ylabel('RMSF (nm)')
