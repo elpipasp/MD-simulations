3f1b39394f9f335688f63a5984a343ca2ade2d57
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-02-23 18:33:07 +0000

    hbonds_pie.py

---
+++ hbonds_pie.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+data_file = "D20_D21_HBONDShbonds-details_sorted.dat"
+labels = []
+occupancies = []
+
+with open(data_file, 'r') as file:
+    next(file)  #skip the header
+    for line in file:
+        line = line.strip().split('\t')
+        donor = line[0]
+        acceptor = line[1]
+        occupancy = float(line[2].rstrip('%'))  #remove % and convert to float
+        if occupancy > 3.0:
+            label = f"{donor}-{acceptor}"
+            labels.append(label)
+            occupancies.append(occupancy)
+
+
+fig, ax = plt.subplots(figsize=(8, 8))
+colors = plt.cm.tab20c(range(len(occupancies)))
+patches, texts, autotexts = ax.pie(occupancies, labels=labels, startangle=140, colors=colors, autopct='%1.1f%%')
+
+#set text label properties
+for text, color in zip(texts, colors):
+    text.set_color(color)  #set label color to match slice color
+    text.set_fontsize(14)
+    text.set_fontweight('bold') 
+ax.set_title('Hydrogen Bond Occupancy (%)', pad=20, fontsize=16)
+plt.axis('equal')  
+plt.show()
