4e744a6cc99cf5b18b6857a6b4c1bbcc765d7be5
elpipasp <113988540+elpipasp@users.noreply.github.com>
2024-03-18 22:00:49 +0000

    PCA.py

--- PCA.py
+++ PCA.py
@@ -7,10 +7,11 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from collections import Counter
 from sklearn.cluster import DBSCAN
 
-traj_files = ['D21.dcd', 'D22.dcd']
+traj_files = ['D10.dcd', 'D11.dcd', 'D12.dcd', 'D13.dcd', 'D14.dcd', 'D15.dcd', 'D16.dcd', 'D17.dcd', 'D18.dcd', 'D19.dcd', 'D20.dcd', 'D21.dcd', 'D22.dcd',]
 traj = md.load(traj_files, top='ROS_1_SC.pdb')
 
-peptide_residues = range(1, 17) 
+#specify residue range for the peptide or N-ter
+peptide_residues = range(1, 17) #1,17 and seganame PROA was used for PCA of receptor N-terminus. segname PROB and resid 1, 10 was used for GnRH PCA 
 selection_string = f'segname PROA and (residue {peptide_residues[0]} to {peptide_residues[-1]})'
 peptide_traj = traj.atom_slice(traj.topology.select(selection_string))
 
@@ -23,51 +24,54 @@ peptide_pcs = pca.fit_transform(peptide_coords)
 x = peptide_pcs[:, 0]
 y = peptide_pcs[:, 1]
 kde = gaussian_kde(np.vstack([x, y]))
-
-#evaluate the KDE on a grid
 x_grid, y_grid = np.mgrid[x.min():x.max():100j, y.min():y.max():100j]
 z = kde(np.vstack([x_grid.ravel(), y_grid.ravel()]))
 
-# Step 7: Perform DBSCAN clustering
-dbscan = DBSCAN(eps=0.1, min_samples=10) 
-cluster_labels = dbscan.fit_predict(peptide_pcs)
+#################### comment for gpcr residues //// only uncoment for segname PROB selection (peptide) ####################
+#perform DBSCAN clustering
+#dbscan = DBSCAN(eps=0.1, min_samples=3)
+#cluster_labels = dbscan.fit_predict(peptide_pcs)
 
 #count the number of frames in each cluster
-cluster_counter = Counter(cluster_labels)
+#cluster_counter = Counter(cluster_labels)
 
-# sort clusters based on population
-sorted_clusters = sorted(cluster_counter.items(), key=lambda x: x[1], reverse=True)
+#sort clusters based on population
+#sorted_clusters = sorted(cluster_counter.items(), key=lambda x: x[1], reverse=True)
 
 #print frame ranges of the three most populated clusters
-for i in range(3):
-    label, count = sorted_clusters[i]
-    frames = np.where(cluster_labels == label)[0]
-    print(f'Cluster {label}: Frame range [{frames.min()}, {frames.max()}], Number of frames: {count}')
-
+#for i in range(3):
+   # label, count = sorted_clusters[i]
+   # frames = np.where(cluster_labels == label)[0]
+   # print(f'Cluster {label}: Frame range [{frames.min()}, {frames.max()}], Number of frames: {count}')
+########################################################################################################################
 #plot
-fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+fig, axs = plt.subplots(1, 2, figsize=(12, 10), gridspec_kw={'width_ratios': [6, 2]})  # Adjusted figsize and width_ratios
+#density Plot
 im = axs[0].pcolormesh(x_grid, y_grid, z.reshape(x_grid.shape), shading='auto', cmap='viridis')
-axs[0].set_xlabel('PCA 1', fontsize=16, fontweight='bold')
-axs[0].set_ylabel('PCA 2', fontsize=16, fontweight='bold')
-axs[0].tick_params(axis='both', which='both', length=0, labelsize=14) 
+axs[0].set_xlabel('PCA 1', fontsize=30, fontweight='bold')
+axs[0].set_ylabel('PCA 2', fontsize=30, fontweight='bold')
+axs[0].set_title('ROS-1 N-ter 1 to 17', fontsize=28, fontweight='bold')
+axs[0].tick_params(axis='both', which='both', length=8, labelsize=22)
 divider = make_axes_locatable(axs[0])
-cax = divider.append_axes("bottom", size="5%", pad=0.5)
+cax = divider.append_axes("bottom", size="5%", pad=1)
 cbar = plt.colorbar(im, cax=cax, orientation='horizontal')
-cbar.ax.set_xlabel('Population Density', fontsize=12, fontweight='bold')
-cbar.set_ticks([]) 
+cbar_ticks = [z.min(), z.max()]
+cbar.set_ticks(cbar_ticks)
+cbar.set_ticklabels(['Low', 'High'])
+cbar.ax.set_xlabel('Population Density', fontsize=24, fontweight='bold')
+cbar.ax.tick_params(axis='both', which='both', labelsize=22)
 
-#Variance Explained plot
+#scree Plot (Variance Explained)
 variance_explained = pca.explained_variance_ratio_
-bars = axs[1].bar(range(1, len(variance_explained) + 1), variance_explained, align='center', color=['forestgreen', 'blue']) 
-axs[1].set_xlabel('Principal Component', fontsize=16, fontweight='bold')
-axs[1].set_ylabel('Variance Explained', fontsize=16, fontweight='bold')
+bars = axs[1].bar(range(1, len(variance_explained) + 1), variance_explained, align='center', color=['forestgreen', 'blue'])
+axs[1].set_xlabel('PCA', fontsize=30, fontweight='bold')
+axs[1].set_ylabel('Variance Explained', fontsize=30, fontweight='bold')
 axs[1].set_xticks([1, 2])
 axs[1].set_yticks([])
-axs[1].tick_params(axis='both', which='both', length=0, labelsize=14)
+axs[1].tick_params(axis='both', which='both', length=8, labelsize=22)
 
 for i, bar in enumerate(bars):
     height = bar.get_height()
-    axs[1].text(bar.get_x() + bar.get_width()/2., height, '{:.1%}'.format(variance_explained[i]), ha='center', va='bottom', fontsize=14)
-
-plt.tight_layout(rect=[0, 0, 0.9, 0.95])
+    axs[1].text(bar.get_x() + bar.get_width()/2., height, '{:.1%}'.format(variance_explained[i]), ha='center', va='bottom', fontsize=22)
+plt.tight_layout()
 plt.show()
